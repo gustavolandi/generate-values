2013aafc548bf20cc820d292782f34aed9600ac1
Gustavo Landi <gustavo.landi1992@hotmail.com>
2021-10-11 13:41:19 -0300

    base64 component encoding and decoding

--- src/app/base-64/base-64.component.ts
+++ src/app/base-64/base-64.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'base-64',
@@ -7,13 +8,29 @@ import { Component, OnInit } from '@angular/core';
   })
   export class Base64Component implements OnInit {
     
-    constructor(){
-
+    mobileQuery: MediaQueryList;
+    textToEncode : string = '';
+    textToDecode : string = '';
+    control : boolean = false;
+  
+    private _mobileQueryListener: () => void;
+      
+    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+      this.mobileQuery = media.matchMedia('(max-width: 600px)');
+      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+      this.mobileQuery.addListener(this._mobileQueryListener);
     }
 
     ngOnInit(): void {
         
     }
 
+    encodeBase64(){
+      this.textToDecode = btoa(this.textToEncode);
+    }
+
+    decodeBase64(){
+      this.textToEncode = atob(this.textToDecode);
+    }
 
   }
