110f59ee929ffb8cf0230c8b663091c0d89a0c21
Gustavo Landi <gustavo.landi1992@hotmail.com>
2021-10-16 10:39:35 -0300

    adding jwt decoding header and payload

--- src/app/jwt/jwt.component.ts
+++ src/app/jwt/jwt.component.ts
@@ -1,9 +1,11 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ExportFileService } from '../service/export-file.service';
-import { ExportFileModel } from '../service/model/ExportFileModel';
-import { FileParams } from '../service/model/FileParams';
 import { SharedService } from '../service/shared.service';
+
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+const REGEX_JWT = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]*$/;
 
 @Component({
     selector: 'jwt',
@@ -13,13 +15,20 @@ import { SharedService } from '../service/shared.service';
   export class JWTComponent implements OnInit {
     
     mobileQuery: MediaQueryList;
-    textToEncode : string = '';
-    textToDecode : string = '';
+    private _mobileQueryListener: () => void;
+    
+    jwtEncoded : string = '';
+
+    jwtDecodedHeader : string = '';
+    jwtDecodedPayload : string = '';
+
+    jwtDecodedSignature : string = '';
+    jwtDecodedSignaturePrivateKey : string = '';
+    jwtDecodedSignaturePublicKey : string = '';
+
     control : boolean = false;
     fileContent: string | ArrayBuffer | null = '';
-    private _mobileQueryListener: () => void;
-    multipleLines : boolean = false;
-    multipleLinesToConvertFile : boolean = false;
+
     convertFileOptions = [{text: 'Encode', id : 1 }, {text: 'Decode', id: 2}];
     convertFileSelect : number = 1;
     controlDownloadFile : boolean = true;
@@ -27,7 +36,7 @@ import { SharedService } from '../service/shared.service';
     @ViewChild('fileInputConvert') fileInputConvert!: ElementRef;
     @ViewChild('fileInputEncode') fileEncode!: ElementRef;
       
-    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, 
+    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
       private exportFile : ExportFileService,
       private sharedService : SharedService) {
       this.mobileQuery = media.matchMedia('(max-width: 600px)');
@@ -39,16 +48,69 @@ import { SharedService } from '../service/shared.service';
         
     }
 
-    
+    jwtDecode(){
+
+      if (this.jwtEncoded === '') {
+        this.jwtDecodedHeader = '';
+        this.jwtDecodedPayload = '';
+        this.jwtDecodedSignature = '';
+        this.jwtDecodedSignaturePrivateKey = '';
+        this.jwtDecodedSignaturePublicKey = '';
+        return;
+      }
+
+      if (this.jwtEncoded.match(REGEX_JWT)) {
+
+        try {
+           const jwtHelperService = new JwtHelperService();
+          
+          const decodedToken = jwtHelperService.decodeToken(this.jwtEncoded);
+          
+          this.jwtDecodedPayload = JSON.stringify(decodedToken, undefined, 4);
+
+          let decodedHeader = JSON.stringify(atob(this.jwtEncoded.split('.')[0]));
+          decodedHeader = decodedHeader.split('\\').join('');
+          decodedHeader = decodedHeader.substring(1,decodedHeader.length-1);
+          
+          const jsonTokenHeader = JSON.parse(decodedHeader);
+          this.jwtDecodedHeader =  JSON.stringify(jsonTokenHeader, undefined, 4);
+        } catch (e) {
+          console.log(e);
+        }
+
+      } else {
+        
+      }
+    }
+
+    validateJwt(){
+      const jwtHelperService = new JwtHelperService();
+      const expirationDate = jwtHelperService.getTokenExpirationDate(this.jwtEncoded);
+      const isExpired = jwtHelperService.isTokenExpired(this.jwtEncoded);
+    }
+     
+
+    encodeBase64(){
+    }
+
+    decodeBase64() {
+
+    }
+
+    readFile(event: any) {
+     
+    }
+
+    readAndConvertFile(event: any) {
+     
+    }
+
+    downloadFile(type: string) {
+     
+    }
 
     copyValue(valueToCopy : string) {
-      if (this.textToEncode != '') {
-        if (valueToCopy === 'encode') {
-          this.sharedService.copyValue(this.textToEncode);
-        } else if (valueToCopy === 'decode') {
-          this.sharedService.copyValue(this.textToDecode);
-        }
-      }
+      
     }
 
   }
